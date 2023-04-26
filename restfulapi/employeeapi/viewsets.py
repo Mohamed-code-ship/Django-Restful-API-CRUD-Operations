b21fa7735e45c2de1ff5243620d798a2ec9667a4
Mohacode <mohaabdul568@gmail.com>
2023-04-26 21:51:38 +0300

    initial

---
+++ restfulapi/employeeapi/viewsets.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from . import models
+from . import serializers
+
+class EmployeeViewset(viewsets.ModelViewSet):
+    queryset = models.Employee.objects.all()
+    serializer_class = serializers.EmployeeSerializer
+    
+  
+        
+
+
---
+++ restfulapi/restfulapi/router.py
@@ -0,0 +1,6 @@
+from employeeapi.viewsets import EmployeeViewset
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('employee' , EmployeeViewset)
